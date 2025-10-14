fcc6695d1bb50b13fd15941f2f315eb12fde7581
AlthafMhdh <Alf11althaf@gmail.com>
2025-10-15 01:54:07 +0530

    project updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,33 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Dashboard from './admin/dashboard'
 import Home from './client/home'
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
 import Login from './admin/login';
 import ProtectedRoute from './routes/ProtectedRoute';
-import { auth } from './firebase/config';
+//import { auth } from './firebase/config';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setIsLoggedIn(!!user);
-    });
-    return () => unsubscribe();
-  }, []);
+  // useEffect(() => {
+  //   const unsubscribe = auth.onAuthStateChanged((user) => {
+  //     setIsLoggedIn(!!user);
+  //   });
+  //   return () => unsubscribe();
+  // }, []);
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route 
-          path="/admin" 
+        <Route path="/admin" 
           element={
             <ProtectedRoute>
               <Dashboard />
             </ProtectedRoute>
           } 
         />
-        <Route 
-          path="/login" 
-          element={<Login/>} 
+        <Route path="/login" element={<Login/>} 
         />
       </Routes>
     </BrowserRouter>
